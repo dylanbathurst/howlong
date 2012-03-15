2ddb164b703cccc7f6258459985a89ac76f5bebf
dylanbathurst <dylanbathurst@gmail.com>
2012-03-14 17:05:10 -0700

    initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+var timeago = require('./lib/timeago');
+
+// var timeStamp = new Date(1327019856116); // 54 days ago
+// var timeStamp = new Date(); // Just now
+
+var d = new Date();
+
+console.log(timeago.howLong(1327019856116));
